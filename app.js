0b030067991bedf832558496649dabeb5352319e
Arslan <muhammadarslanrajput12@gmail.com>
2024-03-12 09:53:56 -0700

    javascript

---
+++ app.js
@@ -0,0 +1,14 @@
+let tableName=+prompt("Type your table Name");
+let num1=(1)
+
+document.write("<br />"+tableName+" x "+ num1++ +" = "+tableName*num1++)
+
+document.write("<br />"+tableName+"  x "+ num1+" = "+tableName*num1++)
+document.write("<br />"+tableName+"  x "+ num1+" = "+tableName*num1++)
+document.write("<br />"+tableName+"  x "+ num1+" = "+tableName*num1++)
+document.write("<br />"+tableName+"  x "+ num1+" = "+tableName*num1++)
+document.write("<br />"+tableName+"  x "+ num1+" = "+tableName*num1++)
+document.write("<br />"+tableName+"  x "+ num1+" = "+tableName*num1++)
+document.write("<br />"+tableName+"  x "+ num1+" = "+tableName*num1++)
+document.write("<br />"+tableName+"  x "+ num1+" = "+tableName*num1++)
+document.write("<br />"+tableName+"  x "+ num1+" = "+tableName*num1++)
